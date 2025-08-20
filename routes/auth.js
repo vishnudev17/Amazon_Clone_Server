656ece6414e2f378251fafcb7819cacea867837a
vishnudev17 <vishnudev.m2001@gmail.com>
2025-08-20 15:44:48 +0530

    latest commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const authRouter = express.Router();
+//signUp
 
 authRouter.post("/api/signup/", async (req, res) => {
   try {
@@ -24,6 +26,28 @@ authRouter.post("/api/signup/", async (req, res) => {
     res.status(500).json({ error: e.message });
 
   }
+});
+//SignIn
+
+authRouter.post('/api/signIn/', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ msg: 'User with this email doesn\'t exist!' });
+    }
+    const isPasswordMatch = await bcryptjs.compare(password, user.password);
+    if (!isPasswordMatch) {
+      res.status(400).json({ msg: 'Incorrect password' });
+    }
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+    res.json({ token, ...user._doc });
+
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+
+  }
+
 });
 
 module.exports = authRouter;
